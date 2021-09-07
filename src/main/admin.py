b14378c751472914e0b5962b26f29238ec3eced8
zanzydnd <zanzydnd@gmail.com>
2021-09-07 14:04:37 +0300

    ip address validation

--- src/main/admin.py
+++ src/main/admin.py
@@ -1,3 +1,6 @@
+import re
+
+from django.core.exceptions import ValidationError
 from django.forms import ModelForm
 from django.contrib import admin
 
@@ -7,12 +10,12 @@ from main.services.hash_service import jwt_string
 
 class DatabaseForm(ModelForm):
 
-    def save(self, commit=True):
-        database = super(DatabaseForm, self).save(commit=False)
-        database.db_password = jwt_string(self.cleaned_data['db_password'])
-        if commit:
-            database.save()
-        return database
+    def clean(self):
+        cleaned_data = super().clean()
+        if not re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", cleaned_data['db_ip']):
+            raise ValidationError(
+                "Incorrect ip address"
+            )
 
     class Meta:
         model = Database
