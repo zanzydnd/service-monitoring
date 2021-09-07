b602621dc1a3c60341c5d0b2c8413a23d2d29060
zanzydnd <zanzydnd@gmail.com>
2021-09-08 01:31:14 +0300

    percentage

--- src/main/dto.py
+++ src/main/dto.py
@@ -2,15 +2,15 @@ class DatabaseDTO():
     def __init__(self, **kwargs):
         self.database = kwargs['database']
         self.last_results = kwargs['last_results']
-        status_int = self.last_results.count()
+        self.status_int = 0
         self.count = self.last_results.count()
         for result in self.last_results:
-            if result.colour == "orange":
-                status_int -= 0.5
-            elif result.colour == "red":
-                status_int -= 1
+            if result.colour == "#2fcc66":
+                self.status_int += 1
+
+        self.status_int = self.status_int / self.last_results.count()
 
         self.status = "Впорядке"
-        if status_int <= self.last_results.count() // 2:
+        if self.status_int <= 0.5:
             self.status = "Сбои"
-
+        self.status_int *= 100
